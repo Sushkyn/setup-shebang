15431ccd0cc2be806582dbd44dd6974212b7649f
Sushkyn <70662829+Sushkyn@users.noreply.github.com>
2026-04-21 22:55:03 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
-if [ "$(id -u)" -ne 0 ]; then
-  echo -e "This script must be run as root, use 'sudo' or run it as root."
-  exit 1
+KEYMAP="us"
+loadkeys $KEYMAP
+REGION_CITY="Europe/Moscow"
+HOST="localhost"
+USERNAME="shebang"
+ROOT_PASSWORD=""
+pass1=""
+pass2=""
+DISK=""
+
+if grep -q "GenuineIntel" /proc/cpuinfo; then
+    cpu="intel-ucode"
+elif grep -q "AuthenticAMD" /proc/cpuinfo; then
+    cpu="amd-ucode"
 fi
 
-[ ! -d /sys/firmware/efi ] && echo -e "System not booted in UEFI mode." && exit 1
-
-case "$(readlink -f /sbin/init)" in
-  *"runit"*)
-    INIT="runit" && echo -e "Init system: $INIT"
-    ;;
-  *)
-    echo -e "Init system: Not supported." && exit 1
-    ;;
-esac
-
-case "$(grep vendor /proc/cpuinfo)" in
-  *"Intel"*)
-    UCODE="intel-ucode"
-    ;;
-  *"AMD"*)
-    UCODE="amd-ucode"
-    ;;
-esac
-
-confirm_password() {
+password() {
   stty -echo
   until [ "$pass1" = "$pass2" ] && [ "$pass1" ] && [ "$pass2" ]; do
     printf "\n%s\n" "$1" >&2 && read -p $"> " pass1
@@ -46,37 +37,8 @@ sed -i -e s"/\#CleanMethod.*/CleanMethod = KeepCurrent/"g /etc/pacman.conf
 # Dependencies
 command -v parted >/dev/null 2>&1 || pacman -Sy --needed --noconfirm --disable-download-timeout parted
 
-# Load keymap
-until [ "$KEYMAP" ]; do
-  clear
-  echo -e "Load keymap (default: us)" && read -p $"> " KEYMAP
-  [ ! "$KEYMAP" ] && KEYMAP="us"
-  loadkeys $KEYMAP || setxkbmap $KEYMAP
-done
-
-# Timezone
-until [ "$REGION_CITY" ]; do
-  clear
-  echo -e "Local time (default: Europe/Moscow)" && read -p $"> " REGION_CITY
-  [ ! "$REGION_CITY" ] && REGION_CITY="Europe/Moscow"
-done
-
-# Host
-until [ "$HOST" ]; do
-  clear
-  echo -e "Hostname (default: localhost)" && read -p $"> " HOST
-  [ ! "$HOST" ] && HOST="localhost"
-done
-
-# Username
-until [ "$USERNAME" ]; do
-  clear
-  echo -e "Username (default: shebang)" && read -p $"> " USERNAME
-  [ ! "$USERNAME" ] && USERNAME="shebang"
-done
-
 # Root
-[ ! "$ROOT_PASSWORD" ] && ROOT_PASSWORD=$(confirm_password "Password for superuser (will use same for root)")
+[ ! "$ROOT_PASSWORD" ] && ROOT_PASSWORD=$(password "Password for superuser (will use same for root)")
 
 # Choose disk
 until [ -e "$DISK" ]; do
@@ -101,14 +63,7 @@ esac
 ROOT_PART=$PART2
 
 # Encrypt
-until [ "$ENCRYPTED" ]; do
-  clear
-  echo -e "Encrypt filesystem (y/default: n)" && read -p $"> " ENCRYPTED
-  [ ! "$ENCRYPTED" ] && ENCRYPTED="n"
-  if [ "$ENCRYPTED" = "y" ]; then
-    [ ! "$CRYPTPASS" ] && CRYPTPASS=$(confirm_password "Password for encryption (must at least 6 characters)")
-  fi
-done
+CRYPTPASS=$(password "Password for encryption (must at least 6 characters)")
 
 # Partition disk
 clear
@@ -116,10 +71,8 @@ swapoff -a
 umount -AR /mnt*
 cryptsetup close /dev/mapper/root
 
-if [ "$ENCRYPTED" = "y" ]; then
-  dd if=/dev/zero of=$DISK bs=2M status=progress && sync -f || sync -f
-  dd if=/dev/urandom of=$DISK bs=2M status=progress && sync -f || sync -f
-fi
+dd if=/dev/zero of=$DISK bs=2M status=progress && sync -f || sync -f
+dd if=/dev/urandom of=$DISK bs=2M status=progress && sync -f || sync -f
 
 parted -s "$DISK" mklabel gpt
 parted -s "$DISK" mkpart primary fat32 1MiB 512MiB
@@ -127,25 +80,21 @@ parted -s "$DISK" mkpart primary ext4 512MiB 100%
 parted -s "$DISK" set 1 boot on
 
 # Encrypt drive
-if [ "$ENCRYPTED" = "y" ]; then
-  echo -ne "$CRYPTPASS" | cryptsetup -q luksFormat --pbkdf=pbkdf2 "$ROOT_PART"
-  echo -ne "$CRYPTPASS" | cryptsetup open "$ROOT_PART" root
+echo -ne "$CRYPTPASS" | cryptsetup -q luksFormat --pbkdf=pbkdf2 "$ROOT_PART"
+echo -ne "$CRYPTPASS" | cryptsetup open "$ROOT_PART" root
 
-  ROOT_PART="/dev/mapper/root"
-fi
+ROOT_PART="/dev/mapper/root"
 
 # Format and mount partitions
 mkfs.fat -F 32 "$PART1"
 fatlabel "$PART1" ESP
 mkfs.ext4 -L root -F -O ^quota,^has_journal,^metadata_csum,^ext_attr,^extra_isize,uninit_bg,fast_commit -b2048 -m1 "$ROOT_PART"
 mount "$ROOT_PART" /mnt
-shred -z -f /mnt/*
 mkdir -p /mnt/boot/efi
 mount "$PART1" /mnt/boot/efi
 # Create swapfile
-SWAP_SIZE=$(echo $(($(free -g | awk '/^Mem:/{print $2}') * 2 + 4)))
 mkdir /mnt/swap
-fallocate -l "$SWAP_SIZE"G /mnt/swap/swapfile
+fallocate -l  12G /mnt/swap/swapfile
 chmod 600 /mnt/swap/swapfile
 mkswap /mnt/swap/swapfile
 swapon /mnt/swap/swapfile
@@ -154,37 +103,19 @@ swapon /mnt/swap/swapfile
 clear
 echo -e 'Done with configuration. Installing...'
 
-if [ "$ENCRYPTED" = "y" ]; then
-  basestrap /mnt base $INIT elogind-$INIT efibootmgr dbus-$INIT dhcpcd-$INIT grub $UCODE wpa_supplicant-$INIT cryptsetup-$INIT
-else
-  basestrap /mnt base $INIT elogind-$INIT efibootmgr dbus-$INIT dhcpcd-$INIT grub $UCODE wpa_supplicant-$INIT
-fi
+basestrap /mnt base runit elogind-runit efibootmgr dbus-runit dhcpcd-runit grub $cpu wpa_supplicant-runit cryptsetup-runit
 
-basestrap /mnt linux-zen linux-zen-headers linux-firmware mkinitcpio
+basestrap /mnt linux-hardened linux-hardened-headers linux-firmware mkinitcpio
 
 fstabgen -U /mnt >/mnt/etc/fstab
 
 # Chroot
-(INIT="$INIT" PART2="$PART2" ROOT_PASSWORD="$ROOT_PASSWORD" ENCRYPTED="$ENCRYPTED" REGION_CITY="$REGION_CITY" HOST="$HOST" USERNAME="$USERNAME" KEYMAP="$KEYMAP" artix-chroot /mnt /bin/bash -c 'bash <(curl -s https://raw.githubusercontent.com/shebang-linux/setup-shebang/main/deploy.sh); exit')
-
-# Save connection
-if [[ ! -f /etc/wpa_supplicant/wpa_supplicant.conf ]]; then
-  echo -e 'update_config=1
-ap_scan=1
-fast_reauth=1
-network={
-ssid="replaceme"
-psk="replaceme"
-scan_ssid=1
-}' >/mnt/etc/wpa_supplicant/wpa_supplicant.conf
-else
-  cp /etc/wpa_supplicant/wpa_supplicant.conf /mnt/etc/wpa_supplicant/wpa_supplicant.conf
-fi
+mkdir -p /mnt/root/zartix
+cp /root/zartix/deploy.sh /mnt/root/zartix/
+chmod +x /mnt/root/zartix/deploy.sh
+(PART2="$PART2" ROOT_PASSWORD="$ROOT_PASSWORD" REGION_CITY="$REGION_CITY" HOST="$HOST" USERNAME="$USERNAME" KEYMAP="$KEYMAP" artix-chroot /mnt /bin/bash -c 'bash /root/shebang-linux/deploy.sh; exit')
 
 # Perform finish
 swapoff -a
 umount -AR /mnt*
 cryptsetup close "$ROOT_PART"
-
-clear
-echo -e 'Installation completed successfully. You may now reboot or poweroff...'
